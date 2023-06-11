807b4e891b914d70fd2ef7bc739cd740849abaa5
unknown <surajtelakar10@gmail.com>
2023-06-11 12:56:14 +0530

    first commit

---
+++ Jenkins-Project/src/main/java/Jenkins_project/Jenkins_Projecct/App.java
@@ -0,0 +1,13 @@
+package Jenkins_project.Jenkins_Projecct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
